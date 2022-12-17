2fec5aa47846c5159da2665e192dc1b10d425619
Sridhar Ratnakumar <srid@srid.ca>
2022-12-17 14:35:43 -0500

    Init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.mission-control.flakeModule
+      ];
+      perSystem = { pkgs, lib, config, ... }: {
+        mission-control.scripts = {
+          hello = {
+            description = "Say Hello";
+            command = "echo Hello";
+          };
+          fmt = {
+            description = "Format the top-level Nix files";
+            command = "${lib.getExe pkgs.nixpkgs-fmt} ./*.nix";
+            category = "Tools";
+          };
+          ponysay = {
+            package = pkgs.ponysay;
+          };
+        };
+        devShells.default =
+          let shell = pkgs.mkShell { };
+          in config.mission-control.installToDevShell shell;
+      };
+    };
+}
